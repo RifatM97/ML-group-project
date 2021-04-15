7415629be963787667bad01be796326641e4e180
rwshaw <rwshaw97@gmail.com>
2021-04-15 16:03:18 +0100

    requirements

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 
 import preprocessing as prep
 import methods
 
-import matplotlib.pyplot as plt
 
 def preprocessing(df):
     prep.sex2binary(df)
--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,4 @@
 import pandas as pd
-import numpy as np
-import random as rnd
-import matplotlib.pyplot as plt
-import sklearn
-import seaborn as sns
-from sklearn.model_selection import train_test_split
 
 
 def import2df(file):
